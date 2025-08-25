c074dfcddb08bf834a391451ed007740691b7d54
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2025-08-25 22:20:57 +0530

    fixed some stupid errors and made matrix print + gen dynamic to number of columns (even tho its single variable, the transpose still has different dimensions so it will be needed for matrix multiplication next

--- lin_reg.c
+++ lin_reg.c
@@ -12,35 +12,31 @@ typedef struct array {
 
 typedef struct matrix {
   int rows;
+  int columns;
   array *data[];
 } matrix;
 
 void printMatrix(matrix *mat) {
   for (int i = 0; i < mat->rows; i++) {
-    for (int j = 0; j < 2; j++) {
-      printf("%i, ", mat->data[j]->data[i]);
+    for (int j = 0; j < mat->columns; j++) {
+      printf("%i, ", mat->data[i]->data[j]);
     }
     printf("\n");
   }
 }
 
-matrix *randMatrixGen(int rows) {
-  matrix *mat =
-      malloc(sizeof(matrix) +
-             2 * sizeof(array *)); // allocate space for 2 column pointers
+matrix *randMatrixGen(int rows, int columns) {
+  matrix *mat = malloc(sizeof(matrix) + rows * sizeof(array *));
   mat->rows = rows;
-
-  // allocate the two column arrays (each with 'rows' ints)
-  mat->data[0] = malloc(sizeof(array) + rows * sizeof(int));
-  mat->data[0]->columns = rows;
-  mat->data[1] = malloc(sizeof(array) + rows * sizeof(int));
-  mat->data[1]->columns = rows;
+  mat->columns = columns;
 
   srand(time(NULL));
 
   for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < 2; j++) {
-      mat->data[j]->data[i] = rand() % (MAX - MIN + 1) + MIN;
+    mat->data[i] = malloc(sizeof(array) + columns * sizeof(int));
+    mat->data[i]->columns = columns;
+    for (int j = 0; j < columns; j++) {
+      mat->data[i]->data[j] = rand() % (MAX - MIN + 1) + MIN;
     }
   }
 
@@ -56,34 +52,30 @@ int arrMean(array *arr) {
 }
 
 matrix *transpose(matrix *mat) {
-  matrix *finalMat =
-      malloc(sizeof(matrix) +
-             mat->rows * sizeof(array *)); // allocate header + pointers
-  finalMat->rows = mat->data[0]->columns;  // == mat->rows
+  matrix *finalMat = malloc(sizeof(matrix) + mat->columns * sizeof(array *));
+  finalMat->rows = mat->columns;
+  finalMat->columns = mat->rows;
 
   for (int i = 0; i < finalMat->rows; i++) {
-    finalMat->data[i] =
-        malloc(sizeof(array) +
-               2 * sizeof(int)); // each transposed column has 2 elements
-    finalMat->data[i]->columns = 2;
-    finalMat->data[i]->data[0] = mat->data[0]->data[i];
-    finalMat->data[i]->data[1] = mat->data[1]->data[i];
+    finalMat->data[i] = malloc(sizeof(array) + finalMat->columns * sizeof(int));
+    finalMat->data[i]->columns = finalMat->columns;
+    for (int j = 0; j < finalMat->columns; j++) {
+      finalMat->data[i]->data[j] = mat->data[j]->data[i];
+    }
   }
   return finalMat;
 }
 
 int main(void) {
-
-  matrix *mat = randMatrixGen(10);
+  matrix *mat = randMatrixGen(10, 2);
   printMatrix(mat);
 
-  printf("\nX Mean: %i", arrMean(mat->data[0]));
-  printf("\nY Mean: %i", arrMean(mat->data[1]));
-  printf("\n");
+  printf("\nX Mean: %i\n", arrMean(mat->data[0]));
+  printf("\nY Mean: %i\n", arrMean(mat->data[1]));
 
   matrix *T = transpose(mat);
-  printf("Element 1, 9 = %i", T->data[9]->data[1]);
+  printf("\nTransposed:\n");
+  printMatrix(T);
 
-  printf("\n");
   return 0;
 }
