556d7bda1a2a4d515de0d596c52600f514bb45d3
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2025-08-25 22:57:26 +0530

    changed confusing array struct naming (columns to size) and added columns attr. to matrix (makes sense even if the array size would match). implemented matrix mul function

--- lin_reg.c
+++ lin_reg.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 int MIN = 0;
-int MAX = 200;
+int MAX = 50;
 
 typedef struct array {
-  int columns;
+  int size;
   int data[];
 } array;
 
@@ -34,7 +34,7 @@ matrix *randMatrixGen(int rows, int columns) {
 
   for (int i = 0; i < rows; i++) {
     mat->data[i] = malloc(sizeof(array) + columns * sizeof(int));
-    mat->data[i]->columns = columns;
+    mat->data[i]->size = columns;
     for (int j = 0; j < columns; j++) {
       mat->data[i]->data[j] = rand() % (MAX - MIN + 1) + MIN;
     }
@@ -45,10 +45,10 @@ matrix *randMatrixGen(int rows, int columns) {
 
 int arrMean(array *arr) {
   int sum = 0;
-  for (int i = 0; i < arr->columns; i++) {
+  for (int i = 0; i < arr->size; i++) {
     sum += arr->data[i];
   }
-  return sum / arr->columns;
+  return sum / arr->size;
 }
 
 matrix *transpose(matrix *mat) {
@@ -58,11 +58,31 @@ matrix *transpose(matrix *mat) {
 
   for (int i = 0; i < finalMat->rows; i++) {
     finalMat->data[i] = malloc(sizeof(array) + finalMat->columns * sizeof(int));
-    finalMat->data[i]->columns = finalMat->columns;
+    finalMat->data[i]->size = finalMat->columns;
     for (int j = 0; j < finalMat->columns; j++) {
       finalMat->data[i]->data[j] = mat->data[j]->data[i];
     }
   }
+  return finalMat;
+}
+
+matrix *matMul(matrix *mat1, matrix *mat2) {
+  matrix *finalMat = malloc(sizeof(matrix) + mat1->rows * sizeof(array *));
+  finalMat->rows = mat1->rows;
+  finalMat->columns = mat2->columns;
+
+  for (int i = 0; i < finalMat->rows; i++) {
+    finalMat->data[i] = malloc(sizeof(array) + finalMat->columns * sizeof(int));
+    finalMat->data[i]->size = finalMat->columns;
+    for (int j = 0; j < finalMat->columns; j++) {
+      int sum = 0;
+      for (int k = 0; k < mat1->columns; k++) {
+        sum += mat1->data[i]->data[k] * mat2->data[k]->data[j];
+      }
+      finalMat->data[i]->data[j] = sum;
+    }
+  }
+
   return finalMat;
 }
 
@@ -76,6 +96,10 @@ int main(void) {
   matrix *T = transpose(mat);
   printf("\nTransposed:\n");
   printMatrix(T);
+
+  matrix *product = matMul(mat, T);
+  printf("\nProduct:\n");
+  printMatrix(product);
 
   return 0;
 }
