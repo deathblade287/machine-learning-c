14d98d34af6e84e43f2e5984300381f36d26b239
Aviral Dhingra <aviral.dhingra.2008@gmail.com>
2025-09-01 22:56:01 +0530

    some progress with structure of defining equations in python wo using string literals too much

--- calc.py
+++ calc.py
@@ -1,30 +1,47 @@
-class eq:
-    def __init__(self, eq: str):
-        self.eq = eq
-        self.symbols = []
-        for char in eq:
-            if char.isalpha():
-                self.symbols.append(symbol(char))
-
-class symbol:
-    def __init__(self, symb: str):
-        if (type(symb) is not str or len(symb) != 1):
-            raise ValueError("The symbol has to be a single charecter string. Please check type and length")
-
-        self.symb = symb
+class expression:
+    def __init__(self, term1, term2, operator):
+        self.term1 = term1
+        self.term2 = term2
+        self.operator = operator
 
     def __repr__(self):
-        return str(self.symb)
+        return f"{self.term1} {self.operator} {self.term2}"
+    
+    def __add__(self, e2):
+        return expression(self, e2, '+')
+    
+    def __sub__(self, e2):
+        return expression(self, e2, '-')
 
-    def __add__(self, s):
-        return f"{self.symb} + {s.symb}"
-    def __pow__(self, p):
-        return f"{self.symb}**{p}"
 
-x = symbol("x")
-y = symbol("y")
-print(x + y)
-print(x**2)
+class variable:
+    def __init__(self, symbol: str, power: int = 1):
+        if (type(symbol) is not str or len(symbol) != 1):
+            raise ValueError("The symbol has to be a single charecter string. Please check type and length")
 
-equation = eq("x + y = 10")
-print(equation.symbols)
+        self.symbol = symbol
+        self.power = power
+
+    def __repr__(self):
+        return f"{str(self.symbol)}**{self.power}"
+
+    def __mul__(self, s):
+        return expression(self, s, "*")
+
+    def __pow__(self, p: int):
+        return variable(self.symbol, p)
+    
+    def __add__(self, v2):
+        return expression(self, v2, '+')
+    
+    def __sub__(self, v2):
+        return expression(self, v2, '-')
+
+x = variable("x")
+y = variable("y")
+
+y = x**2 + x
+z = y + 5
+print(z.term1)
+print(z.term2)
+print(z.operator)
